e98074cbe035760bbd5656cfc0869028e9cae8e8
vnikhil955 <vnikhil955.0@gmail.com>
2021-09-29 14:50:31 +0530

    removes eval and creates new functions

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,3 +1,4 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,18 +8,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
   result: any = "";
+  operator: any = "";
+  value1: any = "";
+  value2: any = "";
+  status: boolean = false;
+  solve_status: boolean = false;
+  counter: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  dis(val: any) {
-    this.result += val;
+  get(val: any) {
+    if (this.solve_status == true) {
+      this.result = "";
+      this.solve_status = false;
+    }
+    if (this.status == false) {
+      this.result += val;
+      this.value1 = this.result;
+    }
+    else {
+      this.result = "";
+      this.result += val;
+      this.value2 = this.result;
+      this.counter = true;
+    }
+  }
+
+  operators(val: any) {
+    this.operator = val;
+    this.status = true;
+  }
+
+  solution() {
+    if (this.counter == true) {
+      this.solve();
+      this.counter = false;
+      this.value1 = this.result;
+    }
   }
 
   solve() {
-    this.result = eval(this.result);
+    this.status = false;
+    this.solve_status = true;
+    if (this.operator == '-') {
+      this.result = this.value1 - this.value2;
+    }
+    else if (this.operator == '*') {
+      this.result = this.value1 * this.value2;
+    }
+    else if (this.operator == '/') {
+      this.result = this.value1 / this.value2;
+    }
+    else {
+      this.result = +this.value1 + +this.value2;
+    }
+
+    console.log('result=' + this.result);
+    console.log('value1=' + this.value1);
+    console.log('value2=' + this.value2);
   }
 
   clr() {
